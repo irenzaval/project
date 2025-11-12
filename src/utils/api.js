c4464d96918121c88923cb7c3e2d4031795dd781
irinazavaley <elenazavaley1979@gmail.com>
2025-11-12 20:33:11 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,19 @@
+// utils/api.js
+export const submitForm = async (formData) => {
+  const response = await fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      access_key: 'YOUR_ACCESS_KEY',
+      ...formData
+    })
+  });
 
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
+};
